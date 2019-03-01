04346fc3946bb2f7dbe6cbaead34d9b0ec534212
Wei <chef27615@gmail.com>
2019-03-01 12:14:08 -0500

    classes and Obj

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -60,7 +60,7 @@ console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -138,6 +138,11 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+
+zooAnimals.forEach(function(cv){
+  return animalNames.push(cv.animal_name);
+})
+
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -147,6 +152,11 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 */
 
 const lowerCase = [];
+
+zooAnimals.map(function(cv){
+  return lowerCase.push(cv.scientific_name.toLowerCase());
+})
+
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -155,6 +165,13 @@ The zoos are concenred about animals with a lower population count. Find out whi
 
 */
 const largerPopulation = [];
+
+zooAnimals.filter(function(cv){
+  if(cv.population<5){
+    return largerPopulation.push(cv);
+  }
+})
+
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -162,7 +179,9 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce(function(ac, cv){
+  return ac= ac+cv.population;
+ }, 0);
 console.log(populationTotal);
 
 
